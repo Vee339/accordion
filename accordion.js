c17ab7957ca149d8c10bd196544ade3e4a2d40b7
Vee339 <81091817+Vee339@users.noreply.github.com>
2024-12-15 13:26:39 -0500

    This is the first commit

--- accordion.js
+++ accordion.js
@@ -1,1 +1,17 @@
+let sections = document.getElementsByTagName("a");
 
+var divsToHide = document.getElementsByClassName("accordion-section");
+
+for (const section of sections) {
+  section.addEventListener("click", function () {
+    for (var i = 0; i < divsToHide.length; i++) {
+      divsToHide[i].style.display = "none";
+    }
+    let content = document.getElementById(section.className);
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
